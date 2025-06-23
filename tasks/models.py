7699aca72e1a0941cb96b6ba52790fa62904b054
Sahalu Aminu <sahlminkok7@gmail.com>
2025-06-23 18:52:26 +0100

    Create tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    PRIORITY = [
+        ('LOW', 'Low'),
+        ('MEDIUM', 'Medium'),
+        ('HIGH', 'High'),
+    ]
+    STATUS = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    due_date = models.DateField()
+    priority = models.CharField(max_length=30, choices=PRIORITY)
+    status = models.CharField(max_length=30, choices=STATUS)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = "tasks"
+  
