9f864d643c4ff6a35b80985749163ff454f98b89
Sahalu Aminu <sahlminkok7@gmail.com>
2025-06-27 17:57:14 +0100

    Add a create task view

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
     tasks = Task.objects.all()
@@ -10,3 +11,15 @@ def detail(request, pk):
     task = Task.objects.get(id=pk)
     context = { 'task': task }
     return render(request, 'tasks/detail.html', context)
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TaskForm()
+
+    context = { 'form': form }
+    return render(request, 'tasks/create_task.html', context)
