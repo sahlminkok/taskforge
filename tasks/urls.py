9c995447a3e0e902365b93414c787327e3c32b8d
Sahalu Aminu <sahlminkok7@gmail.com>
2025-06-27 17:58:27 +0100

    Add create task path

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:pk>/', views.detail, name="detail")
+    path('<int:pk>/', views.detail, name="detail"),
+    path('create/', views.create_task, name="create_task")
 ]
