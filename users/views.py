47316b0cbdefcb168efbc5e8be440464470bac84
Sahalu Aminu <sahlminkok7@gmail.com>
2025-06-28 17:03:59 +0100

    Add login and logout views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('tasks:index')
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid username or password"
+            })
+
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
